903d04a60d5e146730076b8e280114713d2c16ad
maybeddi <diana06bg20@mail.ru>
2026-08-28 00:02:28 +0300

    feat(calculator) add button slots and state management

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <Qstring>
 //обьявление переменных, сигналов и функций кнопок. Тут нет логики
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +18,16 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
 
+private slots:
+    void onDigitClicked(int digit); //слот для цифр
+    void onOperationClicked(const Qstring &op); // слот для операций
+    void onEqualsClicked(); // слот для равно
+    void onDeleteClicked(); // слот для удаления
+    void onDotClicked(); // слот для точки
 private:
     Ui::MainWindow *ui;
+
+    double currentValue = 0.0;
+    Qstring currentOperation = "";
 };
 #endif // MAINWINDOW_H
