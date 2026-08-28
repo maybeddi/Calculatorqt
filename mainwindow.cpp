6c7b9480cd29bed70d986fa03744f229e8b59ad8
maybeddi <diana06bg20@mail.ru>
2026-08-28 22:27:56 +0300

    feat(calculator): added for buttons 'Operations' and 'exlusive' connecting signals to slots

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,5 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "QKeyEvent"
 #include "QDebug" // для отладки (вывод в консоль)
 // логика калькулятора,он приводит в действие все то, что описано в файле .h(пример: свзязывание кнопок,обработка нажатий )
 MainWindow::MainWindow(QWidget *parent)
@@ -37,4 +36,24 @@ void MainWindow::setupConnections()
         });
 
     }
+    // подключение сигнала clicked для операций
+    connect(ui->pushButtonPlus,&QPushButton::clicked,
+            this,[this](){onOperationClicked("+"); });
+    connect(ui->pushButtonMinus,&QPushButton::clicked,
+            this,[this](){onOperationClicked("-"); });
+    connect(ui->pushButtonMultiply,&QPushButton::clicked,
+            this,[this](){onOperationClicked("*"); });
+    connect(ui->pushButtonDivide,&QPushButton::clicked,
+            this,[this](){onOperationClicked(":"); });
+    connect(ui->pushButtonPercent,&QPushButton::clicked,
+            this,[this](){onOperationClicked("%"); });
+
+    //особые кнопки
+    connect(ui->pushButtonDot,&QPushButton::clicked,
+            this,&MainWindow::onDotClicked);
+    connect(ui->pushButtonDelete,&QPushButton::clicked,
+            this,&MainWindow::onDeleteClicked);
+    connect(ui->pushButtonEquals,&QPushButton::clicked,
+            this,&MainWindow::onEqualsClicked);
+
 }
